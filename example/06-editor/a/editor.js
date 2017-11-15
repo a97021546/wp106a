9fb443d3952628d5dc10da904b51a5c70418e12b
a97021546 <31914012+a97021546@users.noreply.github.com>
2017-11-15 14:24:46 +0800

    Add files via upload

--- example/06-editor/a/editor.js
+++ example/06-editor/a/editor.js
@@ -37,6 +37,64 @@ const template = [
           var text = document.getElementById('text')
           fs.writeFile(fileName, text.value)
         }
+      },
+      {
+        label:'New file',
+        accelerator: 'CmdOrCtrl+N',
+        click: function () {
+          let content = "Some text to save into the file";
+          
+          // You can obviously give a direct path without use the dialog (C:/Program Files/path/myfileexample.txt)
+          dialog.showSaveDialog((fileName) => {
+              if (fileName === undefined){
+                  console.log("You didn't save the file");
+                  return;
+              }
+          
+              // fileName is a string that contains the path and filename created in the save file dialog.  
+              fs.writeFile(fileName, content, (err) => {
+                  if(err){
+                      alert("An error ocurred creating the file "+ err.message)
+                  }
+           
+                  alert("The file has been succesfully saved and open");
+                  var filePath = document.getElementById('filePath')
+                  filePath.innerText = fileName
+                  fs.readFile(fileName.toString(), 'utf8', function (err, data) {
+                    if (err) window.alert('read fail!')
+                    var text = document.getElementById('text')
+                    text.value = data
+                    
+                  }) 
+              })
+          }) 
+        }
+      },
+      {
+        label:'Save As New File',
+        accelerator: 'CmdOrCtrl+Shift+S',
+        click: function () {
+          var fileName = document.getElementById('filePath').innerText
+          if (fileName.trim().length === 0) window.alert('No file loaded!')
+          var text = document.getElementById('text')
+          fs.writeFile(fileName, text.value)
+          
+          let content = "Some text to save into the file"
+          dialog.showSaveDialog((fileName) => {
+            if (fileName === undefined){
+                console.log("You didn't save the file");
+                return;
+            }
+        
+            // fileName is a string that contains the path and filename created in the save file dialog.  
+            fs.writeFile(fileName, content, (err) => {
+                if(err){
+                    alert("An error ocurred creating the file "+ err.message)
+                }
+
+            })
+        })
+        }
       }
     ]
   },
